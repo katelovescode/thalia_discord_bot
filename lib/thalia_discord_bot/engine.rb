ef9b6f7ccd3519d2b0475544f89468fcdeb82536
Kate <kate@katelovescode.com>
2024-05-18 08:33:17 -0500

    Installs and configures rspec

--- lib/thalia_discord_bot/engine.rb
+++ lib/thalia_discord_bot/engine.rb
@@ -1,5 +1,9 @@
 module ThaliaDiscordBot
   class Engine < ::Rails::Engine
     isolate_namespace ThaliaDiscordBot
+    
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
